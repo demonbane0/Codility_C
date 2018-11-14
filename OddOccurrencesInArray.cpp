a2d215079daf7cc868ff73e67cee0e85cf2e87cc
demonbane <demonbane777@gmail.com>
2018-11-14 14:07:06 +0800

    OddOccurrencesInArray 100
    OddOccurrencesInArray 100 (XOR)

--- OddOccurrencesInArray.cpp
+++ OddOccurrencesInArray.cpp
@@ -1,27 +1,15 @@
 // you can use includes, for example:
 // #include <algorithm>
-#include <map>
-using namespace std;
+
 // you can write to stdout for debugging purposes, e.g.
 // cout << "this is a debug message" << endl;
 
 int solution(vector<int> &A) {
-    map<int,int> hashmap;
-    map<int,int>::iterator iter;
-    for(unsigned int ii=0;ii<A.size();ii++)
-    {
-        iter=hashmap.find(A[ii]);
-     
-        if(iter==hashmap.end())
-        {
-  
-            hashmap[A[ii]]=ii;
-        }
-        else
-        {
-            hashmap.erase(iter);
-        }
-    }
-    return hashmap.begin()->first;
     // write your code in C++14 (g++ 6.2.0)
+    long long remain=(1+A.size()+1)*(A.size()+1)/2;
+    for (int ind1=0;ind1<A.size();ind1++)
+    {
+        remain-=(long long)A[ind1];
+    }
+    return (int)remain;
 }
