e2d0e604a3778978dcfe076d193c10d252934bcb
demonbane <demonbane777@gmail.com>
2018-11-23 17:04:56 +0800

    FrogJmp 100
    double 100, 
    float 44

---
+++ FrogJmp.cpp
@@ -0,0 +1,10 @@
+// you can use includes, for example:
+// #include <algorithm>
+#include <math.h>
+// you can write to stdout for debugging purposes, e.g.
+// cout << "this is a debug message" << endl;
+
+int solution(int X, int Y, int D) {
+    // write your code in C++14 (g++ 6.2.0)
+    return ceil((double)(Y-X)/(double)D);
+}
