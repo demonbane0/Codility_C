75e0dc22c0a37cc2c9774cd655c2959bb97db748
demonbane <demonbane777@gmail.com>
2018-11-23 18:36:57 +0800

    MaxCounters 100
    Using temp base value and only update array values when it is increased or at last

--- MaxCounters.cpp
+++ MaxCounters.cpp
@@ -1,5 +1,5 @@
 // you can use includes, for example:
-// #include <algorithm>
+ #include <algorithm>
 
 // you can write to stdout for debugging purposes, e.g.
 // cout << "this is a debug message" << endl;
@@ -8,17 +8,23 @@
     // write your code in C++14 (g++ 6.2.0)
     vector<int>Ans(N,0);
     int MaxValue=0;
+    int LastBase =0;
     for (unsigned int idx1=0;idx1<A.size();idx1++)
     {
      if(A[idx1]==N+1)
      {
-        Ans.assign(N,MaxValue);
+        LastBase=MaxValue;
      }
      else
      {
+         Ans[A[idx1]-1]=max(Ans[A[idx1]-1],LastBase);
          Ans[A[idx1]-1]++;
-         MaxValue=MaxValue>Ans[A[idx1]-1]? MaxValue: Ans[A[idx1]-1];
+         MaxValue=max(MaxValue,Ans[A[idx1]-1]);
      }
+    }
+    for (int idx1=0;idx1<N;idx1++)
+    {
+        Ans[idx1]=max(LastBase,Ans[idx1]);
     }
     return Ans;
 }
