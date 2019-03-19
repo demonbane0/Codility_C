7ffe5637433fc25b54bdf8a60e2839e1cf258ef7
demonbane <demonbane777@gmail.com>
2019-03-19 11:37:18 +0800

    Dominator 91
    extreme_empty_and_single_item 
    empty and single element arrays ✘WRONG ANSWER 
    got -1, but dominator exists, for example on position 0

---
+++ Dominator.cpp
@@ -0,0 +1,21 @@
+#include <unordered_map>
+using namespace std;
+int solution(vector<int> &A) {
+    // write your code in C++14 (g++ 6.2.0)
+    if(A.size()==0) return -1;
+    unordered_map<int,unsigned int> num_map;
+    unordered_map<int,unsigned int>::iterator it;
+    for (unsigned int idx=0;idx<A.size();idx++)
+    {
+        if((it=num_map.find(A[idx]))==num_map.end())
+        {
+            num_map.insert(make_pair(A[idx],1));
+        }
+        else
+        {
+            it->second++;
+            if(it->second>A.size()/2) return idx;
+        }
+    }
+    return -1;
+}
