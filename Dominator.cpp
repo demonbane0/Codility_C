f8f10f00f57c532d738a2ed7d7b3b4ab7ef46a0f
demonbane <demonbane777@gmail.com>
2019-03-19 11:42:32 +0800

    Dominator 100
    unordered_map
    O(N) or O(N*log(N))

--- Dominator.cpp
+++ Dominator.cpp
@@ -3,6 +3,8 @@ using namespace std;
 int solution(vector<int> &A) {
     // write your code in C++14 (g++ 6.2.0)
     if(A.size()==0) return -1;
+    else if(A.size()==1) return 0;
+    
     unordered_map<int,unsigned int> num_map;
     unordered_map<int,unsigned int>::iterator it;
     for (unsigned int idx=0;idx<A.size();idx++)
